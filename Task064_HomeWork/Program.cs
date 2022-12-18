4b52840288b904b886d6666697a44fa74a2d18aa
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-18 12:55:03 +0800

    add Task 064 HomeWork

---
+++ Task064_HomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿// // Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+//натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+
+Clear();
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Write(1);
+
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Write(input);
+    int output = Convert.ToInt32(ReadLine());
+    return output;
+}
+
+
